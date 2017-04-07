286e140ee4c91883177740fe15718041ba259512
chapmanb <chapmanb@50mail.com>
2017-04-07 05:37:53 -0400

    Remove bucket region from S3 specifications
    Toil determines bucket regions using boto API and us-east-1 lookups of
    buckets in any region

--- NA12878-chr20/run_toil_aws.sh
+++ NA12878-chr20/run_toil_aws.sh
@@ -4,7 +4,7 @@ set -eu -o pipefail
 
 PROJECT=NA12878-platinum-chr20
 LEADER_PRIVATE_IP=10.0.0.XX
-JOB_STORE=aws:us-east-1:bcbio-NA12878-chr20-jobstore
+JOB_STORE=aws:us-east-1:bcbio-na12878-chr20-jobstore
 NODE_TYPE=m4.4xlarge
 SPOT_BID=0.8
 MAX_NODES=2
@@ -16,5 +16,4 @@ cwltoil \
   --nodeType $NODE_TYPE --maxNodes $MAX_NODES \
   --jobStore $JOB_STORE \
   $PROJECT-workflow/main-$PROJECT.cwl \
-  $PROJECT-cwl-workflow/main-$PROJECT-samples.json
-  
+  $PROJECT-workflow/main-$PROJECT-samples.json

1a487f3b4419cf85ffc1d8f6edd8489602a3a0cf
chapmanb <chapmanb@50mail.com>
2017-04-12 08:12:37 -0400

    Clean up creation of both Arvados and S3 CWL

--- NA12878-chr20/run_generate_cwl_arvados.sh
+++ NA12878-chr20/run_generate_cwl_arvados.sh
@@ -4,4 +4,5 @@ set -eu -o pipefail
 SYSTEM_YAML=bcbio_system_arvados.yaml
 bcbio_vm.py template --systemconfig $SYSTEM_YAML germline_template.yaml NA12878-platinum-chr20.csv
 bcbio_vm.py cwl --systemconfig $SYSTEM_YAML NA12878-platinum-chr20/config/NA12878-platinum-chr20.yaml
+rm -rf NA12878-platinum-chr20-workflow-arvados
 mv NA12878-platinum-chr20-workflow NA12878-platinum-chr20-workflow-arvados
--- NA12878-chr20/run_generate_cwl_s3.sh
+++ NA12878-chr20/run_generate_cwl_s3.sh
@@ -3,3 +3,5 @@ set -eu -o pipefail
 
 bcbio_vm.py template --systemconfig bcbio_system_s3.yaml germline_template.yaml NA12878-platinum-chr20.csv
 bcbio_vm.py cwl --systemconfig bcbio_system_s3.yaml NA12878-platinum-chr20/config/NA12878-platinum-chr20.yaml
+rm -rf NA12878-platinum-chr20-workflow-s3
+mv NA12878-platinum-chr20-workflow NA12878-platinum-chr20-workflow-s3

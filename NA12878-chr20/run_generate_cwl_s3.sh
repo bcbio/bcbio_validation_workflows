15fe72c086118e58ae0d97b2d38a785cb1e70720
chapmanb <chapmanb@50mail.com>
2017-05-01 05:57:32 -0400

    Local JSON input file for running on Synapse
    Support for DREAM challenge infrastructure

--- NA12878-chr20/run_generate_cwl_s3.sh
+++ NA12878-chr20/run_generate_cwl_s3.sh
@@ -5,3 +5,5 @@ bcbio_vm.py template --systemconfig bcbio_system_s3.yaml germline_template.yaml 
 bcbio_vm.py cwl --systemconfig bcbio_system_s3.yaml NA12878-platinum-chr20/config/NA12878-platinum-chr20.yaml
 rm -rf NA12878-platinum-chr20-workflow-s3
 mv NA12878-platinum-chr20-workflow NA12878-platinum-chr20-workflow-s3
+mkdir -p NA12878-platinum-chr20-workflow-local
+sed 's#s3://biodata/#biodata/#' NA12878-platinum-chr20-workflow-s3/main-NA12878-platinum-chr20-samples.json > NA12878-platinum-chr20-workflow-local/main-NA12878-platinum-chr20-samples.json

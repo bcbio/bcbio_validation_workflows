bfe7de749fb565987f180685fadfe9431531548f
chapmanb <chapmanb@50mail.com>
2017-05-15 13:41:07 -0400

    GA4GH: fix data pointers to work with synapse
    Ensure biodata directory relative to original CWL (in a sub directory).
    Remove old resource files in JSON.

--- NA12878-chr20/run_generate_cwl_s3.sh
+++ NA12878-chr20/run_generate_cwl_s3.sh
@@ -8,4 +8,4 @@ bcbio_vm.py template --systemconfig bcbio_system_s3.yaml germline_template.yaml 
 bcbio_vm.py cwl --systemconfig bcbio_system_s3.yaml NA12878-platinum-chr20/config/NA12878-platinum-chr20.yaml
 mkdir -p NA12878-platinum-chr20-workflow-s3
 mv NA12878-platinum-chr20-workflow/*-samples.json NA12878-platinum-chr20-workflow-s3
-sed 's#s3://biodata/#biodata/#' NA12878-platinum-chr20-workflow-s3/main-NA12878-platinum-chr20-samples.json > NA12878-platinum-chr20-workflow/main-NA12878-platinum-chr20-samples.json
+sed 's#s3://biodata/#../biodata/#' NA12878-platinum-chr20-workflow-s3/main-NA12878-platinum-chr20-samples.json > NA12878-platinum-chr20-workflow/main-NA12878-platinum-chr20-samples.json

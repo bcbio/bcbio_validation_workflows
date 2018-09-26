3c23c7595798d6faeb04be1516d35f71802f3069
chapmanb <chapmanb@50mail.com>
2018-09-26 19:35:00 +0000

    Aravados: update script for running via WES

--- wes-agha-test/run_arvados.sh
+++ wes-agha-test/run_arvados.sh
@@ -5,4 +5,8 @@ PID=qr1hi-j7d0g-a68mg4e5oh37oek
 PNAME=wes_chr21_test
 PLATFORM=arvados
 
-bcbio_vm.py cwlrun arvados $PNAME-workflow-$PLATFORM -- --project-uuid $PID
+WES_API_HOST=wes.qr1hi.arvadosapi.com
+WES_API_AUTH=5vipdwj62f3wz6avnih8p27krsdpngphgymyukw79nslnxk29x
+
+bcbio_vm.py cwlrun wes --host $WES_API_HOST --auth $WES_API_AUTH $PNAME-workflow-$PLATFORM
+#bcbio_vm.py cwlrun arvados $PNAME-workflow-$PLATFORM -- --project-uuid $PID
